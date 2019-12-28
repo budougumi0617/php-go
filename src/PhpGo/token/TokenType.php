6bd923dcd1646eea13b82a59666203e5debbe212
budougumi0617 <budougumi0617@gmail.com>
2019-12-28 23:00:43 +0900

    :sparkles: add new types

--- src/PhpGo/token/TokenType.php
+++ src/PhpGo/token/TokenType.php
@@ -38,6 +38,9 @@ abstract class TokenType
     public const T_ASSIGN = "ASSIGN"; // =
     public const T_ADD = "ADD"; // +
     public const T_SUB = "SUB"; // -
+
+    public const T_DEFINE = "DEFINE";  // :=
+
     public const T_LPAREN = "LPAREN";// (
     public const T_LBRACK = "LBRACK";// [
     public const T_LBRACE = "LBRACE";// {
@@ -50,4 +53,36 @@ abstract class TokenType
     public const T_SEMICOLON = "SEMICOLON";// ;
     public const T_COLON = "COLON";  // :
     // operator_end
+
+    // keyword_beg
+    public const T_BREAK = "BREAK";  // break
+    public const T_CASE = "CASE";  // case
+    public const T_CHAN = "CHAN";  // chan
+    public const T_CONST = "CONST";  // const
+    public const T_CONTINUE = "CONTINUE";  // continue
+
+    public const T_DEFAULT = "DEFAULT";  // default
+    public const T_DEFER = "DEFER";  // defer
+    public const T_ELSE = "ELSE";  // else
+    public const T_FALLTHROUGH = "FALLTHROUGH";  // fallthrough
+    public const T_FOR = "FOR";  // for
+
+    public const T_FUNC = "FUNC";  // func
+    public const T_GO = "GO";  // go
+    public const T_GOTO = "GOTO";  // goto
+    public const T_IF = "IF";  // if
+    public const T_IMPORT = "IMPORT";  // import
+
+    public const T_INTERFACE = "INTERFACE";  // interface
+    public const T_MAP = "MAP";  // map
+    public const T_PACKAGE = "PACKAGE";  // package
+    public const T_RANGE = "RANGE";  // range
+    public const T_RETURN = "RETURN";  // return
+
+    public const T_SELECT = "SELECT";  // select
+    public const T_STRUCT = "STRUCT";  // struct
+    public const T_SWITCH = "SWITCH";  // switch
+    public const T_TYPE = "TYPE";  // type
+    public const T_VAR = "VAR";  // var
+    // keyword_end
 }
