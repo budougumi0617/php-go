6d825293dcfb07236f283d0239fdfb4866784391
budougumi0617 <budougumi0617@gmail.com>
2019-12-31 20:34:07 +0900

    :sparkles: repl: add REPL loop

--- src/PhpGo/Main.php
+++ src/PhpGo/Main.php
@@ -4,17 +4,26 @@ namespace PhpGo;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/Token/Token.php';
+require_once __DIR__ . '/Repl/repl.php';
 
 use PhpGo\Lexer\Lexer;
 use PhpGo\Token\EofType;
+use function Repl\Start;
 
-$input = $argv[1];
-$lexer = new Lexer($input);
-
-while (true) {
-    $tok = $lexer->nextToken();
-    echo $tok->string() . PHP_EOL;
-    if ($tok->type() instanceof EofType) {
-        break;
+if (count($argv) > 1) {
+    $input = $argv[1];
+    $lexer = new Lexer($input);
+    while (true) {
+        $tok = $lexer->nextToken();
+        echo $tok->string() . PHP_EOL;
+        if ($tok->type() instanceof EofType) {
+            break;
+        }
     }
+} else {
+    $user = get_current_user();
+    echo "Hello {$user}! This is the Go language!" . PHP_EOL;
+    echo "Feel free to type in commands" . PHP_EOL;
+
+    Start();
 }
---
+++ src/PhpGo/Repl/repl.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+namespace Repl;
+
+use PhpGo\Lexer\Lexer;
+use PhpGo\Token\EofType;
+
+const PROMPT = '>> ';
+
+function Start(): void
+{
+    while (true) {
+        echo PROMPT;
+        $line = trim(fgets(STDIN));
+        $lexer = new Lexer($line);
+
+        while (true) {
+            $tok = $lexer->nextToken();
+            echo $tok->string() . PHP_EOL;
+            if ($tok->type() instanceof EofType) {
+                break;
+            }
+        }
+    }
+}
