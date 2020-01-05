bc2e7611cf3a61d7d7c1fdbe54d87511a150c3fd
budougumi0617 <budougumi0617@gmail.com>
2020-01-05 12:19:05 +0900

    :white_check_mark: add test when single import for parseImportGenDecl

--- tests/PhpGo/Parser/ParserTest.php
+++ tests/PhpGo/Parser/ParserTest.php
@@ -23,21 +23,45 @@ import (
 EOT;
         $parser = new Parser(new Lexer($input));
         $program = $parser->parseProgram();
-        $this->assertNull($program);
+        $this->assertNotNull($program);
         $this->assertEquals(1, count($program->statements));
         $this->assertTrue($program->statements[0] instanceof GenDecl);
         // cast GenDecl
         $convertFn = fn($obj): GenDecl => $obj;
         $genDecl = $convertFn($program->statements[0]);
-        $this->assertEquals(TokenType::T_IMPORT, $genDecl->token->type);
+        $this->assertEquals(TokenType::T_IMPORT, $genDecl->token->type->getType());
 
         $this->assertEquals(2, count($genDecl->specs));
 
         // cast ImportSpec
         $castImportSpec = fn($obj): ImportSpec => $obj;
         $is = $castImportSpec($genDecl->specs[0]);
-        $this->assertEquals("fmt", $is->path);
+        $this->assertEquals('"fmt"', $is->path->kind->literal);
         $is2 = $castImportSpec($genDecl->specs[1]);
-        $this->assertEquals("log", $is2->path);
+        $this->assertEquals('"log"', $is2->path->kind->literal);
+    }
+
+    public function test_parseProgram_GenDecl_ImportSpec_single(): void
+    {
+        // https://play.golang.org/p/RyvRgMFN79J
+        $input = <<<EOT
+import "log"
+EOT;
+        $parser = new Parser(new Lexer($input));
+        $program = $parser->parseProgram();
+        $this->assertNotNull($program);
+        $this->assertEquals(1, count($program->statements));
+        $this->assertTrue($program->statements[0] instanceof GenDecl);
+        // cast GenDecl
+        $convertFn = fn($obj): GenDecl => $obj;
+        $genDecl = $convertFn($program->statements[0]);
+        $this->assertEquals(TokenType::T_IMPORT, $genDecl->token->type->getType());
+
+        $this->assertEquals(1, count($genDecl->specs));
+
+        // cast ImportSpec
+        $castImportSpec = fn($obj): ImportSpec => $obj;
+        $is = $castImportSpec($genDecl->specs[0]);
+        $this->assertEquals('"log"', $is->path->kind->literal);
     }
 }
