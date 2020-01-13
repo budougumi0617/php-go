a9a6f78a5eb8d18bdb38701644479ae0a9eca947
budougumi0617 <budougumi0617@gmail.com>
2020-01-13 15:10:21 +0900

    :sparkles: ast: add unresolovedObject

--- src/PhpGo/Ast/Object.php
+++ src/PhpGo/Ast/Object.php
@@ -15,4 +15,17 @@ final class GoObject
     // Decl interface{} // corresponding Field, XxxSpec, FuncDecl, LabeledStmt, AssignStmt, Scope; or nil
     // Data interface{} // object-specific data; or nil
     // Type interface{} // placeholder for type information; may be nil
+
+    // The unresolved object is a sentinel to mark identifiers that have been added
+    // to the list of unresolved identifiers. The sentinel is only used for verifying
+    // internal consistency.
+    // var unresolved = new(ast.Object)
+    public const UNRESOLVED_OBJECT = 'UNRESOLVED_OBJECT';
+
+    public static function unresolovedObject(): GoObject
+    {
+        $obj = new GoObject();
+        $obj->name = self::UNRESOLVED_OBJECT;
+        return $obj;
+    }
 }
