2bf532a684b6b9f7d8086c72b3c88d3ae504317f
budougumi0617 <budougumi0617@binc.jp>
2020-01-22 15:00:05 +0900

    :sparkles: add test

---
+++ tests/PhpGo/Evaluator/EvaluatorTest.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace Tests\PhpGo\Evaluator;
+
+final class EvaluatorTest
+{
+
+    public function test_eval_integer_expresion(): void
+    {
+        $got = test
+
+    }
+}
