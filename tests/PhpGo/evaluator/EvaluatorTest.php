4770f3d4f6577a93bf8a52ac3879be5526366fd3
budougumi0617 <budougumi0617@gmail.com>
2020-01-22 15:24:12 +0900

    :white_check_mark: evaluator: add test

--- tests/PhpGo/evaluator/EvaluatorTest.php
+++ tests/PhpGo/evaluator/EvaluatorTest.php
@@ -1,11 +1,40 @@
 <?php declare(strict_types=1);
 
-namespace Tests\PhpGo\evaluator;
+namespace Tests\PhpGo\Evaluator;
 
-final class EvaluatorTest
+use PhpGo\Evaluator\Evaluator;
+use PhpGo\Lexer\Lexer;
+use PhpGo\Object\GoObject;
+use PhpGo\Object\Integer;
+use PhpGo\Parser\Parser;
+use PHPUnit\Framework\TestCase;
+
+final class EvaluatorTest extends TestCase
 {
-    public function test_parseProgram_integer(): void
-    {
 
+    /**
+     * @dataProvider providerForEvalIntegerExpression
+     * @param string $input
+     * @param int $want
+     */
+    public function test_eval_integer_expression(string $input, int $want): void
+    {
+        $got = $this->testEval($input);
+        $integer = Integer::castInteger($got);
+        $this->assertEquals($want, $integer->value);
+    }
+
+    public function providerForEvalIntegerExpression():array {
+        return [
+            'five' => ['5', 5],
+            'ten' => ['10', 10],
+        ];
+    }
+
+    private function testEval(string $input): GoObject
+    {
+        $parser =  new Parser(new Lexer($input));
+        $program = $parser->parseProgram();
+        return Evaluator::Eval($program);
     }
 }
