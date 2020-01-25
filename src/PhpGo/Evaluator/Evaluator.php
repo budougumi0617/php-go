5f4db431964c845e4423e29fbd9e964c6a4eef65
budougumi0617 <budougumi0617@gmail.com>
2020-01-25 11:26:45 +0900

    :sparkles: evaluator: evaluate function

--- src/PhpGo/Evaluator/Evaluator.php
+++ src/PhpGo/Evaluator/Evaluator.php
@@ -6,12 +6,21 @@ use InvalidArgumentException;
 use PhpGo\Ast\AssignStmt;
 use PhpGo\Ast\BasicLit;
 use PhpGo\Ast\BinaryExpr;
+use PhpGo\Ast\BlockStmt;
+use PhpGo\Ast\CallExpr;
 use PhpGo\Ast\ExpressionInterface;
+use PhpGo\Ast\Field;
+use PhpGo\Ast\FieldList;
+use PhpGo\Ast\FuncDecl;
 use PhpGo\Ast\Ident;
 use PhpGo\Ast\NodeInterface;
 use PhpGo\Ast\Program;
+use PhpGo\Object\FunctionObject;
 use PhpGo\Object\GoObject;
 use PhpGo\Object\Integer;
+use PhpGo\Object\Nil;
+use PhpGo\Object\ObjectType;
+use PhpGo\Object\ReturnObj;
 use PhpGo\Object\Scope;
 use PhpGo\Token\DefineType;
 use PhpGo\Token\IntType;
@@ -29,6 +38,9 @@ final class Evaluator
             // FIXME pkg scopeを渡せる
             $program = Program::castProgram($node);
             return self::evalStatements($program->statements, $scope);
+        } else if ($node instanceof BlockStmt) {
+            $blockStmt = BlockStmt::castBlockStmt($node);
+            return self::evalBlockStatement($blockStmt, $scope);
         } else if ($node instanceof Ident) {
             $ident = Ident::castIdent($node);
             return self::evalIdent($ident, $scope);
@@ -58,6 +70,43 @@ final class Evaluator
             } else {
                 throw new UnexpectedValueException("{$stmt->tok} is not support assign type");
             }
+        } else if ($node instanceof FuncDecl) {
+            $func = FuncDecl::castFuncDecl($node);
+            /** @var array<BasicLit> $params */
+            $params = [];
+            // $func->recvには、(x, y int, s string)のような情報が含まれている。
+            foreach ($func->recv->list as $f) {
+                $field = Field::castField($f);
+                // x, y int みたいな引数をパースしている。
+                foreach ($field->names as $name) {
+                    $tok = null;
+                    $ident = Ident::castIdent($name);
+                    // FIXME: 決め打ちで$field->typeをIdentとしている。
+                    switch ($field->type->name){
+                        case 'int':
+                            $tok = new Token(new IntType(), $ident->name);
+                            break;
+                        case 'stringe':
+                            $tok = new Token(new StringType(), $ident->name);
+                            break;
+                        default:
+                            throw  new \BadFunctionCallException(
+                                "cannot support argment type {$field->type->name}"
+                            );
+                    }
+                    $params[]=new BasicLit($tok);
+                }
+            }
+            $funcObj = new FunctionObject($params, $func->body, $scope);
+            if (strlen($func->name->name) > 0) {
+                $scope->set($func->name->name, $funcObj);
+            }
+            return $funcObj;
+        } else if ($node instanceof CallExpr) {
+            $callExpr = CallExpr::castCallExpr($node);
+            $function = self::eval($callExpr, $scope);
+            $args = self::evalExpressions($callExpr->args, $scope);
+            return self::applyFunction($function, $args);
         } else {
             throw new UnexpectedValueException("{$node} is not support node type");
         }
@@ -101,6 +150,68 @@ final class Evaluator
         // FIXME: 束縛関数などが来る（そもそもBasicLitじゃない）可能性もある。
         throw new UnexpectedValueException("createToken: {$expr} is not support type");
     }
+
+    /**
+     * @param array<ExpressionInterface> $args
+     * @param Scope                      $scope
+     *
+     * @return array<GoObject>
+     */
+    private static function evalExpressions(array $args, Scope $scope): array
+    {
+        $results = [];
+        foreach ($args as $arg) {
+            $results[] = self::eval($arg, $scope);
+        }
+        return $results;
+    }
+
+    /**
+     * @param GoObject        $func
+     * @param array<GoObject> $args
+     *
+     * @return GoObject
+     */
+    private static function applyFunction(GoObject $func, array $args): GoObject
+    {
+        $fn = FunctionObject::castFunctionObject($func);
+        $funcScope = self::extendScope($fn, $args);
+        $evaluated = self::eval($fn->body, $funcScope);
+        if ($evaluated instanceof ReturnObj) {
+            $ret = ReturnObj::castReturnObj($evaluated);
+            return $ret->value;
+        }
+        return $evaluated;
+    }
+
+    private static function extendScope(FunctionObject $func, array $args): Scope
+    {
+        $exScope = new Scope($func->scope);
+        foreach ($func->parameters as $i => $param) {
+            $lit = BasicLit::castBasicLit($param);
+            $exScope->set($lit->value, $args[$i]);
+        }
+        return $exScope;
+    }
+
+    /**
+     * @param BlockStmt $stmts
+     * @param Scope     $scope
+     *
+     * @return GoObject
+     */
+    private static function evalBlockStatement(BlockStmt $stmts, Scope $scope): GoObject
+    {
+        $result = new Nil();
+        foreach ($stmts as $stmt) {
+            $result = self::eval($stmt, $scope);
+            if (is_null($result) && $result->type()->getString() === ObjectType::RETURN_OBJ()->getString()) {
+                return $result;
+            }
+        }
+        return $result;
+    }
+
 
     private static function evalIdent(Ident $ident, Scope $scope): GoObject
     {
